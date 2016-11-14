66f498c2e7d3500e101af632f9651326592c2934
josef.sevcik@gmail.com <josef.sevcik@gmail.com>
2016-11-14 13:16:15 +0000

    test

--- libraries/chain/index.cpp
+++ libraries/chain/index.cpp
@@ -21,6 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+
 #include <fc/io/raw.hpp>
 #include <graphene/chain/index.hpp>
 #include <graphene/chain/database.hpp>
