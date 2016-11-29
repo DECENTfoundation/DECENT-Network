4727fe2c81e03bba4bdda388557c6d56e5e6fa61
Josef Sevcik <josef.sevcik@gmail.com>
2016-11-29 16:38:27 +0100

    encryption - wip

--- libraries/encrypt/encryptionutils.cpp
+++ libraries/encrypt/encryptionutils.cpp
@@ -7,25 +7,38 @@
 #include <cryptopp/filters.h>
 #include <cryptopp/files.h>
 #include <cryptopp/ccm.h>
+
 #include <string>
 #include <fstream>
 #include <streambuf>
 #include <fc/log/logger.hpp>
 #include <fc/exception/exception.hpp>
 
+
+
 namespace decent{ namespace crypto{
 AutoSeededRandomPool rng;
 
-std::string d_integer::to_string() const {
+std::string d_integer::to_string() const
+{
 
 
 }
 
-static encryption_results AES_encrypt_file(const std::string &fileIn, const std::string &fileOut, const aes_key &key) {
+
+d_integer d_integer::from_string(std::string from)const
+{
+
+
+}
+
+encryption_results AES_encrypt_file(const std::string &fileIn, const std::string &fileOut, const aes_key &key) {
     try {
+        byte iv[CryptoPP::AES::BLOCKSIZE];
+        memset(iv, 0, sizeof(iv));
         CryptoPP::CBC_Mode<CryptoPP::AES>::Encryption e;
         CryptoPP::FileSink fs(fileOut.c_str(), true);
-        e.SetKey(key.key_byte, CryptoPP::AES::MAX_KEYLENGTH);
+        e.SetKeyWithIV(key.key_byte, CryptoPP::AES::MAX_KEYLENGTH, iv);
         CryptoPP::StreamTransformationFilter* filter=new CryptoPP::StreamTransformationFilter(e, &fs);
 
         const char* file_name = fileIn.c_str();
@@ -42,14 +55,16 @@ static encryption_results AES_encrypt_file(const std::string &fileIn, const std:
     return ok;
 }
 
-static encryption_results AES_decrypt_file(const std::string &fileIn, const std::string &fileOut, const aes_key &key) {
+encryption_results AES_decrypt_file(const std::string &fileIn, const std::string &fileOut, const aes_key &key) {
     try {
-        CryptoPP::CBC_Mode<CryptoPP::AES>::Decryption d;
-        d.SetKey(key.key_byte, CryptoPP::AES::MAX_KEYLENGTH);
-        const char* file_name = fileIn.c_str();
-        CryptoPP::FileSink fs(fileOut.c_str(), true);
-        CryptoPP::StreamTransformationFilter* filter = new CryptoPP::StreamTransformationFilter(d, &fs);
-        CryptoPP::FileSource* fsource = new CryptoPP::FileSource(file_name, true, filter);
+       byte iv[CryptoPP::AES::BLOCKSIZE];
+       memset(iv, 0, sizeof(iv));
+       CryptoPP::CBC_Mode<CryptoPP::AES>::Decryption d;
+       d.SetKeyWithIV(key.key_byte, CryptoPP::AES::MAX_KEYLENGTH, iv);
+       const char* file_name = fileIn.c_str();
+       CryptoPP::FileSink fs(fileOut.c_str(), true);
+       CryptoPP::StreamTransformationFilter* filter = new CryptoPP::StreamTransformationFilter(d, &fs);
+       CryptoPP::FileSource* fsource = new CryptoPP::FileSource(file_name, true, filter);
     } catch (const CryptoPP::Exception &e) {
         elog(e.GetWhat());
         switch (e.GetErrorType()) {
@@ -77,7 +92,7 @@ d_integer get_public_el_gamal_key(const d_integer &privateKey)
     return publicKey;
 }
 
-static encryption_results el_gamal_encrypt(const valtype &message, d_integer &publicKey, valtype &ciphertext)
+encryption_results el_gamal_encrypt(const valtype &message, d_integer &publicKey, ciphertext &result)
 {
     ElGamalKeys::PublicKey key;
     key.AccessGroupParameters().Initialize(EL_GAMAL_MODULUS_512, EL_GAMAL_GROUP_GENERATOR);
@@ -90,7 +105,6 @@ static encryption_results el_gamal_encrypt(const valtype &message, d_integer &pu
         FC_ASSERT(message.size() <= MESSAGE_SIZE);
         copy(message.begin(), message.end(), buffer);
         SecByteBlock messageBytes(buffer, MESSAGE_SIZE);
-        SecByteBlock outcomingCiphertextBytes(EL_GAMAL_CIPHERTEXT_SIZE);
 
         ElGamal::GroupParameters groupParams;
         groupParams.Initialize(EL_GAMAL_MODULUS_512, EL_GAMAL_GROUP_GENERATOR);
@@ -100,14 +114,10 @@ static encryption_results el_gamal_encrypt(const valtype &message, d_integer &pu
 
         CryptoPP::Integer m;
         m.Decode(messageBytes, MESSAGE_SIZE);
-        CryptoPP::Integer d1 = groupParams.ExponentiateBase(randomizer);
-        CryptoPP::Integer c1 = mr.Multiply(m, mr.Exponentiate(publicElement, randomizer));
 
-        groupParams.EncodeElement(true, c1, outcomingCiphertextBytes.BytePtr());
-        groupParams.EncodeElement(true, d1, outcomingCiphertextBytes.BytePtr()+EL_GAMAL_GROUP_ELEMENT_SIZE);
+        result.D1 = groupParams.ExponentiateBase(randomizer);
+        result.C1 = mr.Multiply(m, mr.Exponentiate(publicElement, randomizer));
 
-        ciphertext.reserve(EL_GAMAL_CIPHERTEXT_SIZE);
-        std::copy(outcomingCiphertextBytes.begin(), outcomingCiphertextBytes.end(), std::back_inserter(ciphertext));
     }catch(const CryptoPP::Exception &e) {
         elog(e.GetWhat());
         switch (e.GetErrorType()) {
@@ -120,16 +130,13 @@ static encryption_results el_gamal_encrypt(const valtype &message, d_integer &pu
     return ok;
 }
 
-static encryption_results el_gamal_decrypt(const valtype &ciphertext, d_integer &privateKey, valtype &plaintext)
+encryption_results el_gamal_decrypt(const ciphertext &input, d_integer &privateKey, valtype &plaintext)
 {
     ElGamalKeys::PrivateKey key;
     key.AccessGroupParameters().Initialize(EL_GAMAL_MODULUS_512, EL_GAMAL_GROUP_GENERATOR);
     key.SetPrivateExponent(privateKey);
     try{
-        FC_ASSERT(ciphertext.size() != EL_GAMAL_CIPHERTEXT_SIZE);
-        byte buffer[EL_GAMAL_CIPHERTEXT_SIZE];
-        copy(ciphertext.begin(), ciphertext.end(), buffer);
-        CryptoPP::SecByteBlock ciphertextBytes(buffer, EL_GAMAL_CIPHERTEXT_SIZE);
+
         byte recovered[MESSAGE_SIZE];
         ElGamal::GroupParameters groupParams;
         groupParams.Initialize(EL_GAMAL_MODULUS_512, EL_GAMAL_GROUP_GENERATOR);
@@ -137,15 +144,14 @@ static encryption_results el_gamal_decrypt(const valtype &ciphertext, d_integer 
         FC_ASSERT(privateExponent == privateKey);
         ModularArithmetic mr(groupParams.GetModulus());
 
-        CryptoPP::Integer c1(ciphertextBytes.BytePtr(), (EL_GAMAL_GROUP_ELEMENT_SIZE));
-        CryptoPP::Integer d1(ciphertextBytes.BytePtr() + (EL_GAMAL_GROUP_ELEMENT_SIZE), (EL_GAMAL_GROUP_ELEMENT_SIZE));
-        CryptoPP::Integer s = mr.Exponentiate(d1, privateExponent);
-        CryptoPP::Integer m = mr.Multiply(c1, mr.MultiplicativeInverse(s));
+        CryptoPP::Integer s = mr.Exponentiate(input.D1, privateExponent);
+        CryptoPP::Integer m = mr.Multiply(input.C1, mr.MultiplicativeInverse(s));
         size_t size = m.MinEncodedSize();
 
-        m.Encode(recovered, size);
-        plaintext.reserve(size);
-        std::copy(recovered, recovered+size, std::back_inserter(plaintext));
+        m.Encode(recovered, MESSAGE_SIZE);
+
+        plaintext.reserve(MESSAGE_SIZE);
+        std::copy(recovered, recovered+MESSAGE_SIZE, std::back_inserter(plaintext));
     }catch (const CryptoPP::Exception &e) {
         elog(e.GetWhat());
         switch (e.GetErrorType()) {
@@ -157,6 +163,155 @@ static encryption_results el_gamal_decrypt(const valtype &ciphertext, d_integer 
     }
     return ok;
 }
+
+d_integer hash_elements(ciphertext t1, ciphertext t2, d_integer key1, d_integer key2, d_integer G1, d_integer G2, d_integer G3)
+{
+   CryptoPP::SHA256 hashier;
+   size_t hashSpace =9*(EL_GAMAL_GROUP_ELEMENT_SIZE);
+   hashier.CreateUpdateSpace(hashSpace);
+   byte tmp[EL_GAMAL_GROUP_ELEMENT_SIZE];
+
+   t1.D1.Encode(tmp,EL_GAMAL_GROUP_ELEMENT_SIZE);
+   hashier.Update(tmp, EL_GAMAL_GROUP_ELEMENT_SIZE);
+
+   t1.C1.Encode(tmp,EL_GAMAL_GROUP_ELEMENT_SIZE);
+   hashier.Update(tmp, EL_GAMAL_GROUP_ELEMENT_SIZE);
+
+   t2.D1.Encode(tmp,EL_GAMAL_GROUP_ELEMENT_SIZE);
+   hashier.Update(tmp, EL_GAMAL_GROUP_ELEMENT_SIZE);
+
+   t2.C1.Encode(tmp,EL_GAMAL_GROUP_ELEMENT_SIZE);
+   hashier.Update(tmp, EL_GAMAL_GROUP_ELEMENT_SIZE);
+
+   key1.Encode(tmp,EL_GAMAL_GROUP_ELEMENT_SIZE);
+   hashier.Update(tmp, EL_GAMAL_GROUP_ELEMENT_SIZE);
+
+   key2.Encode(tmp,EL_GAMAL_GROUP_ELEMENT_SIZE);
+   hashier.Update(tmp, EL_GAMAL_GROUP_ELEMENT_SIZE);
+
+   G1.Encode(tmp,EL_GAMAL_GROUP_ELEMENT_SIZE);
+   hashier.Update(tmp, EL_GAMAL_GROUP_ELEMENT_SIZE);
+
+   G2.Encode(tmp,EL_GAMAL_GROUP_ELEMENT_SIZE);
+   hashier.Update(tmp, EL_GAMAL_GROUP_ELEMENT_SIZE);
+
+   G3.Encode(tmp,EL_GAMAL_GROUP_ELEMENT_SIZE);
+   hashier.Update(tmp, EL_GAMAL_GROUP_ELEMENT_SIZE);
+
+   byte digest[32];
+   hashier.Final(digest);
+   CryptoPP::Integer x(digest, 32);
+
+   return x;
+}
+
+bool
+verify_delivery_proof(const delivery_proof &proof, const ciphertext &first, const ciphertext &second,
+                      const d_integer &firstPulicKey, const d_integer &secondPublicKey)
+{
+   ElGamalKeys::PublicKey key1;
+   key1.AccessGroupParameters().Initialize(EL_GAMAL_MODULUS_512, EL_GAMAL_GROUP_GENERATOR);
+   key1.SetPublicElement(firstPulicKey);
+
+   ElGamalKeys::PublicKey key2;
+   key2.AccessGroupParameters().Initialize(EL_GAMAL_MODULUS_512, EL_GAMAL_GROUP_GENERATOR);
+   key2.SetPublicElement(secondPublicKey);
+
+   CryptoPP::Integer x = hash_elements(first, second, firstPulicKey, secondPublicKey, proof.G1, proof.G2, proof.G3);
+
+   ElGamal::GroupParameters groupParams;
+   groupParams.Initialize(EL_GAMAL_MODULUS_512, EL_GAMAL_GROUP_GENERATOR);
+   ModularArithmetic mr(EL_GAMAL_MODULUS_512);
+
+   CryptoPP::Integer subgroupOrder = groupParams.GetSubgroupOrder();
+   CryptoPP::Integer subGroupGenerator = groupParams.GetSubgroupGenerator();
+
+   CryptoPP::Integer t1 = mr.Exponentiate(subGroupGenerator, proof.s);
+   CryptoPP::Integer t2 = mr.Multiply(mr.Exponentiate(key1.GetPublicElement(), x), proof.G1);
+
+   if(t1 != t2)
+      return false;
+   if (mr.Exponentiate(key1.GetGroupParameters().GetSubgroupGenerator(), proof.r) != mr.Multiply(mr.Exponentiate(second.D1, x), proof.G2))
+      return false;
+
+   CryptoPP::Integer c2v = mr.Exponentiate(second.C1, x);
+   CryptoPP::Integer c1v = mr.MultiplicativeInverse(mr.Exponentiate(first.C1, x));
+   CryptoPP::Integer d1v = mr.Exponentiate(first.D1, proof.s);
+   CryptoPP::Integer r2v = mr.Exponentiate(key2.GetPublicElement(), proof.r);
+   CryptoPP::Integer c2vc1v =  mr.Multiply(c2v, c1v);
+   CryptoPP::Integer c2vc1vd1v = mr.Multiply(c2vc1v, d1v);
+
+   return mr.Multiply(c2vc1vd1v, mr.MultiplicativeInverse(r2v)) == proof.G3;
+}
+
+
+
+encryption_results
+encrypt_with_proof(const valtype &message, const d_integer &privateKey, const d_integer &destinationPublicKey,
+                   const ciphertext &incoming, ciphertext &outgoing, delivery_proof &proof)
+{
+   FC_ASSERT(message.size() == MESSAGE_SIZE);
+   try{
+      ElGamalKeys::PrivateKey private_key;
+      private_key.AccessGroupParameters().Initialize(EL_GAMAL_MODULUS_512, EL_GAMAL_GROUP_GENERATOR);
+      private_key.SetPrivateExponent(privateKey);
+
+      ElGamalKeys::PublicKey public_key;
+      public_key.AccessGroupParameters().Initialize(EL_GAMAL_MODULUS_512, EL_GAMAL_GROUP_GENERATOR);
+      public_key.SetPublicElement(destinationPublicKey);
+
+
+      CryptoPP::Integer randomizer(rng, CryptoPP::Integer::One(), EL_GAMAL_MODULUS_512 - 1);
+
+      byte messageBytes[MESSAGE_SIZE];
+      copy(message.begin(), message.end(), messageBytes);
+
+      //encrypt message to outgoing
+      ElGamal::GroupParameters groupParams;
+      groupParams.Initialize(EL_GAMAL_MODULUS_512, EL_GAMAL_GROUP_GENERATOR);
+      ModularArithmetic mr(EL_GAMAL_MODULUS_512);
+      CryptoPP::Integer publicElement = public_key.GetPublicElement();
+      FC_ASSERT(destinationPublicKey == publicElement);
+
+      CryptoPP::Integer m;
+      m.Decode(messageBytes, MESSAGE_SIZE);
+      outgoing.D1 = groupParams.ExponentiateBase(randomizer);
+      outgoing.C1 = mr.Multiply(m, mr.Exponentiate(publicElement, randomizer));
+      //create delivery proof to proofOfDelivery
+      CryptoPP::Integer subgroupOrder = groupParams.GetSubgroupOrder();
+      CryptoPP::Integer subGroupGenerator = groupParams.GetSubgroupGenerator();
+      CryptoPP::Integer privateExponent =  private_key.GetPrivateExponent();
+      CryptoPP::Integer myPublicElement = groupParams.ExponentiateBase(privateExponent);
+
+      CryptoPP::Integer b1(rng, CryptoPP::Integer::One(), subgroupOrder-1);
+      CryptoPP::Integer b2(rng, CryptoPP::Integer::One(), subgroupOrder-1);
+
+      proof.G1 = mr.Exponentiate(subGroupGenerator, b1);
+      proof.G2 = mr.Exponentiate(subGroupGenerator, b2);
+      proof.G3 = mr.Multiply(mr.Exponentiate(incoming.D1, b1), mr.MultiplicativeInverse(mr.Exponentiate(publicElement, b2)));
+
+   /*   ElGamal::Encryptor encryptor(privateKey);
+      const CryptoPP::ElGamalKeys::PublicKey& public_key2 = encryptor.AccessKey();
+
+      FC_ASSERT(public_key == public_key2);*/
+
+      CryptoPP::Integer x = hash_elements(incoming, outgoing, public_key.GetPublicElement(), myPublicElement, proof.G1, proof.G2, proof.G3);
+
+      proof.s = privateExponent * x + b1;
+      proof.r = randomizer * x + b2;
+
+   }catch(const CryptoPP::Exception &e) {
+      elog(e.GetWhat());
+      switch (e.GetErrorType()) {
+         case CryptoPP::Exception::IO_ERROR:
+            return io_error;
+         default:
+            return other_error;
+      }
+   }
+   return ok;
+}
+
 
 
 }}//namespace
--- libraries/encrypt/include/decent/encrypt/encryptionutils.hpp
+++ libraries/encrypt/include/decent/encrypt/encryptionutils.hpp
@@ -8,20 +8,26 @@
 #include <string>
 #include <fc/reflect/variant.hpp>
 #include <cryptopp/osrng.h>
+
+
 using CryptoPP::AutoSeededRandomPool;
 
 #include <cryptopp/secblock.h>
+
 using CryptoPP::SecByteBlock;
 
 #include <cryptopp/elgamal.h>
+
 using CryptoPP::ElGamal;
 using CryptoPP::ElGamalKeys;
 
 #include <cryptopp/cryptlib.h>
+
 using CryptoPP::DecodingResult;
 using CryptoPP::PublicKey;
 
 #include <cryptopp/modarith.h>
+
 using CryptoPP::ModularArithmetic;
 
 #include <cryptopp/pubkey.h>
@@ -29,74 +35,85 @@ using CryptoPP::ModularArithmetic;
 
 #define EL_GAMAL_GROUP_ELEMENT_SIZE 64 //bytes
 #define EL_GAMAL_CIPHERTEXT_SIZE (2 * EL_GAMAL_GROUP_ELEMENT_SIZE) //bytes
-#define MESSAGE_SIZE 33 //bytes
+#define MESSAGE_SIZE 32 //bytes
 #define HASH_SIZE 32 //bytes
 #define PROOF_OF_DELIVERY_SIZE (EL_GAMAL_GROUP_ELEMENT_SIZE * 5 + 2 * (HASH_SIZE +1)) //bytes
-const CryptoPP::Integer EL_GAMAL_MODULUS_512("11760620558671662461946567396662025495126946227619472274601251081547302009186313201119191293557856181195016058359990840577430081932807832465057884143546419.");
+const CryptoPP::Integer EL_GAMAL_MODULUS_512(
+      "11760620558671662461946567396662025495126946227619472274601251081547302009186313201119191293557856181195016058359990840577430081932807832465057884143546419.");
 const CryptoPP::Integer EL_GAMAL_GROUP_GENERATOR(3);
 
 namespace decent {
-    namespace crypto {
-        struct aes_key {
-            unsigned char key_byte[CryptoPP::AES::MAX_KEYLENGTH];
-        };
+namespace crypto {
+struct aes_key {
+   unsigned char key_byte[CryptoPP::AES::MAX_KEYLENGTH];
+};
 
-        enum encryption_results {
-            ok,
-            io_error,
-            other_error,
-        };
+enum encryption_results {
+   ok,
+   io_error,
+   other_error,
+};
 
-        class d_integer : public CryptoPP::Integer {
-        public:
-            std::string to_string()const;
-            d_integer from_string(std::string from)const;
+class d_integer : public CryptoPP::Integer {
+public:
+   std::string to_string() const;
 
-            d_integer(CryptoPP::Integer integer):CryptoPP::Integer(integer){};
-        };
+   d_integer from_string(std::string from) const;
 
-        typedef std::vector<unsigned char> valtype;
+   d_integer(CryptoPP::Integer integer) : CryptoPP::Integer(integer) {}
+};
 
-        struct delivery_proof {
-            d_integer G1;
-            d_integer G2;
-            d_integer G3;
-            d_integer s;
-            d_integer r;
+typedef std::vector<unsigned char> valtype;
 
-            delivery_proof(d_integer g1, d_integer g2, d_integer g3, d_integer s,
-                           d_integer r) : G1(g1), G2(g2), G3(g3), s(s), r(r) {}
-        };
+struct delivery_proof {
+   d_integer G1;
+   d_integer G2;
+   d_integer G3;
+   d_integer s;
+   d_integer r;
+
+   delivery_proof(d_integer g1, d_integer g2, d_integer g3, d_integer s,
+                  d_integer r) : G1(g1), G2(g2), G3(g3), s(s), r(r) {}
+};
+
+struct ciphertext {
+   d_integer C1 = decent::crypto::d_integer(CryptoPP::Integer::One());
+   d_integer D1 = decent::crypto::d_integer(CryptoPP::Integer::One());
+
+};
+
 
 /*********************************************************
  *  Encrypt file wit key
  *********************************************************/
-        static encryption_results AES_encrypt_file(const std::string &fileIn, const std::string &fileOut, const aes_key &key);
+encryption_results AES_encrypt_file(const std::string &fileIn, const std::string &fileOut, const aes_key &key);
 
 /*********************************************************
  *  Decrypt file wit key
  *********************************************************/
-        static encryption_results AES_decrypt_file(const std::string &fileIn, const std::string &fileOut, const aes_key &key);
+encryption_results AES_decrypt_file(const std::string &fileIn, const std::string &fileOut, const aes_key &key);
 
-        static d_integer generate_private_el_gamal_key();
+d_integer generate_private_el_gamal_key();
 
-        static d_integer get_public_el_gamal_key(const d_integer &privateKey);
+d_integer get_public_el_gamal_key(const d_integer &privateKey);
 
-        static encryption_results el_gamal_encrypt(const valtype &message, d_integer &publicKey, valtype &ciphertext);
+encryption_results el_gamal_encrypt(const valtype &message, d_integer &publicKey, ciphertext &result);
 
-        static encryption_results el_gamal_decrypt(const valtype &ciphertext, d_integer &privateKey, valtype &plaintext);
+encryption_results el_gamal_decrypt(const ciphertext &input, d_integer &privateKey, valtype &plaintext);
 
-        static bool
-        encrypt_with_proof(const valtype &message, const valtype privateKey, const valtype &destinationPublicKey,
-                           const valtype &incomingCiphertext, valtype &outcomingCiphertext, valtype &proofOfDelivery);
+encryption_results
+encrypt_with_proof(const valtype &message, const d_integer &privateKey, const d_integer &destinationPublicKey,
+                   const ciphertext &incoming, ciphertext &outgoing, delivery_proof &proof);
 
-        static bool
-        verify_delivery_proof(const valtype &proof, const valtype &firstCiphertext, const valtype &secondCiphertext,
-                              const valtype &firstPulicKey, const valtype &secondPublicKey);
-    }
+bool
+verify_delivery_proof(const delivery_proof &proof, const ciphertext &first, const ciphertext &second,
+                      const d_integer &firstPulicKey, const d_integer &secondPublicKey);
+
 }
+}//namespace
 
 FC_REFLECT_EMPTY(decent::crypto::d_integer)
 
-FC_REFLECT(decent::crypto::aes_key,(key_byte))
-FC_REFLECT(decent::crypto::delivery_proof,(G1)(G2)(G3)(s)(r))
+FC_REFLECT(decent::crypto::aes_key, (key_byte))
+FC_REFLECT(decent::crypto::delivery_proof, (G1)(G2)(G3)(s)(r))
+FC_REFLECT(decent::crypto::ciphertext, (C1)(D1))
