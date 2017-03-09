bd43e60e666c34d7869617c407e95fd7fd88b49c
Josef Sevcik <josef.sevcik@gmail.com>
2017-03-09 18:40:16 +0100

    bugfixes

--- libraries/plugins/seeding/seeding.cpp
+++ libraries/plugins/seeding/seeding.cpp
@@ -75,6 +75,7 @@ void seeding_plugin_impl::handle_content_submit(const operation_history_object &
                });
                //if we run this in main thread it can crash _push_block
                service_thread->async( [cs_op, this, so_id](){
+                    ilog("seeding plugin:  handle_content_submit() lambda called");
                     auto id = package_manager::instance().download_package(cs_op.URI, *this, empty_report_stats_listener::instance());
                     active_downloads[id] = so_id;
                });
