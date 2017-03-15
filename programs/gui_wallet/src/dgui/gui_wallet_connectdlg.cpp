df5fa2b7c23b6d1754742144ca73f8f62df8d1e6
tigran <tigran@blockchainrndhub.com>
2017-03-16 00:55:56 +0400

    wallet_utility and the commented out usage

--- programs/gui_wallet/src/dgui/gui_wallet_connectdlg.cpp
+++ programs/gui_wallet/src/dgui/gui_wallet_connectdlg.cpp
@@ -20,11 +20,113 @@
 #include <stdio.h>
 #include <QMessageBox>
 #include "richdialog.hpp"
+#include "gui_wallet_mainwindow.hpp"
 
 const char* StringFromQString(const QString& a_cqsString)
 {
     QByteArray cLatin = a_cqsString.toLatin1();
     return cLatin.data();
+}
+namespace gui_wallet
+{
+    PasswordDialog::PasswordDialog(Mainwindow_gui_wallet* pParent, bool isSet)
+    : QDialog((QWidget*)pParent, Qt::Window | Qt::WindowTitleHint | Qt::CustomizeWindowHint | Qt::WindowCloseButtonHint)
+    , ret_value()
+    , m_pParent(pParent)
+    , m_main_table(2, 1)
+    {
+        resize(350, 150);
+        
+        m_main_table.horizontalHeader()->hide();
+        m_main_table.verticalHeader()->hide();
+        m_main_table.setShowGrid(false);
+        m_main_table.setFrameShape(QFrame::NoFrame);
+        
+        QPalette plt_tbl = m_main_table.palette();
+        plt_tbl.setColor(QPalette::Base, palette().color(QPalette::Window));
+        m_main_table.setPalette(plt_tbl);
+        
+        DecentButton* unlockButton = new DecentButton();
+        
+        // set hight
+        unlockButton->setFixedHeight(30);
+        
+        if (isSet) {
+            unlockButton->setText("Set Password");
+            unlockButton->setFixedWidth(150);
+        } else {
+            unlockButton->setText("Unlock");
+            unlockButton->setFixedWidth(100);
+        }
+        unlockButton->setFixedHeight(30);
+        unlockButton->setFixedWidth(120);
+        password_box.setEchoMode(QLineEdit::Password);
+        password_box.setAttribute(Qt::WA_MacShowFocusRect, 0);
+        
+        if (isSet) {
+            m_main_table.setCellWidget(0, 0, new QLabel(tr("Choose password to encrypt your wallet.")));
+        } else {
+            m_main_table.setCellWidget(0, 0, new QLabel(tr("Please unlock your wallet to continue.")));
+        }
+        
+        m_main_table.setCellWidget(1, 0, &password_box);
+        //m_main_table.setCellWidget(2, 0, unlockButton);
+        QHBoxLayout* button_layout = new QHBoxLayout();
+        button_layout->addWidget(unlockButton);
+        connect(unlockButton, SIGNAL(LabelClicked()), this, SLOT(unlock_slot()));
+        connect(&password_box, SIGNAL(returnPressed()), unlockButton, SIGNAL(LabelClicked()));
+        
+        m_main_layout.addWidget(&m_main_table);
+        //button_layout->setContentsMargins(50, 0, 50, 0);
+        m_main_layout.addLayout(button_layout);
+        setLayout(&m_main_layout);
+        
+        QTimer::singleShot(0, &password_box, SLOT(setFocus()));
+    }
+    
+    bool PasswordDialog::execRD(QPoint centerPosition, std::string& pass)
+    {
+        ret_value = false;
+        
+        centerPosition.rx() -= size().width() / 2;
+        centerPosition.ry() -= size().height() / 2;
+        
+        QDialog::move(centerPosition);
+        QDialog::exec();
+        pass = password_box.text().toStdString();
+        password_box.setText("");
+        
+        return ret_value;
+    }
+    
+    void PasswordDialog::resizeEvent(QResizeEvent * event)
+    {
+        QSize tableSize = m_main_table.size();
+        m_main_table.setColumnWidth(0, tableSize.width() - 10);
+    }
+    
+    void PasswordDialog::unlock_slot()
+    {
+        std::string str_password = password_box.text().toStdString();
+        auto& pwapi = m_pParent->m_ptr_wallet_utility;
+        try
+        {
+            //QMessageBox::information(this, "Info", str_password.c_str());
+            //  enable below lines and the GUI freezes
+            //pwapi = graphene::wallet_utility::create_wallet_api();
+            //if (pwapi->is_new())
+            //    pwapi->set_password(str_password);
+            //if (pwapi->is_locked())
+            //    pwapi->unlock(str_password);
+        }
+        catch(...)
+        {
+            QMessageBox::critical(this, "Error", "exception");
+        }
+        
+        ret_value = true;
+        close();
+    }
 }
 
 /* ///////////////////////////////////  */
