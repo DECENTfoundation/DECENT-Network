e2ff807086132fe73fc0272271497e0ff305f7ca
iBad <hayk@hayotsyan.com>
2017-05-05 12:16:48 +0200

    Locale is set for input boxes

--- programs/gui_wallet/src/richdialog.cpp
+++ programs/gui_wallet/src/richdialog.cpp
@@ -199,8 +199,11 @@ SendDialog::SendDialog(int a_num_of_text_boxes  , QString title, QString userNam
    m_pTextBoxes[0].setAttribute(Qt::WA_MacShowFocusRect, 0);
    m_pTextBoxes[0].setFixedSize(300, 44);
    m_pTextBoxes[0].setStyleSheet(d_text_box);
+
+   QDoubleValidator* dblValidator = new QDoubleValidator(0.0001, 100000, 4, this);
+   dblValidator->setLocale(_locale);
+   m_pTextBoxes[1].setValidator(dblValidator);
    
-   m_pTextBoxes[1].setValidator(new QDoubleValidator(0.0001, 100000, 4, this));
    m_pTextBoxes[1].setPlaceholderText(QString(tr("Amount")));
    m_pTextBoxes[1].setAttribute(Qt::WA_MacShowFocusRect, 0);
    m_pTextBoxes[1].setFixedSize(300, 44);
