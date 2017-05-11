ebb68cfe89a08ed5231cd58fbf1dd837528efe83
Ghazar Ter-Khachatryan <terxachatryan20@gmail.com>
2017-05-11 19:23:31 +0400

    bug fixed

--- programs/gui_wallet/src/overview_tab.cpp
+++ programs/gui_wallet/src/overview_tab.cpp
@@ -27,55 +27,6 @@
 
 using string = std::string;
 
-class QZebraWidget : public QWidget
-{
-public:
-   QZebraWidget()
-   {
-      m_main_layout.setSpacing(0);
-      m_main_layout.setContentsMargins(0, 0, 0, 0);
-
-      setStyleSheet("background-color:white;");
-      setLayout(&m_main_layout);
-
-#ifdef _MSC_VER
-      int height = style()->pixelMetric(QStyle::PM_TitleBarHeight);
-      setWindowIcon(height > 32 ? QIcon(":/icon/images/windows_decent_icon_32x32.png")
-                    : QIcon(":/icon/images/windows_decent_icon_16x16.png"));
-#endif
-   }
-
-   void AddInfo(QString title, std::string info) {
-      _subWidgets.push_back(new QWidget());
-      _subLayouts.push_back(new QVBoxLayout());
-
-      if (_subWidgets.size() % 2 == 0) {
-         _subWidgets.back()->setStyleSheet("background-color:rgb(244,244,244);");
-      } else {
-         _subWidgets.back()->setStyleSheet("background-color:rgb(255, 255, 255);");
-      }
-
-      QLabel* lblTitle = new QLabel((title));
-//      lblTitle->setStyleSheet("font-weight: bold");
-
-      QLabel* lblInfo = new QLabel(QString::fromStdString(info));
-
-      _subLayouts.back()->setSpacing(0);
-      _subLayouts.back()->setContentsMargins(45,3,0,3);
-      _subLayouts.back()->addWidget(lblTitle);
-      _subLayouts.back()->addWidget(lblInfo);
-
-      _subWidgets.back()->setLayout(_subLayouts.back());
-      m_main_layout.addWidget(_subWidgets.back());
-   }
-private:
-   QVBoxLayout     m_main_layout;
-
-   std::vector<QWidget*>     _subWidgets;
-   std::vector<QVBoxLayout*> _subLayouts;
-   
-};
-
 namespace gui_wallet
 {
 
@@ -241,34 +192,19 @@ void Overview_tab::slot_Details()
       std::string network_fee_percentage = std::to_string(accountInfo["network_fee_percentage"].get<int>());
       std::string lifetime_referrer_fee_percentage = std::to_string(accountInfo["lifetime_referrer_fee_percentage"].get<int>());
       std::string referrer_rewards_percentage = std::to_string(accountInfo["referrer_rewards_percentage"].get<int>());
-      
       std::string name = accountInfo["name"].get<std::string>();
-      QString title = QString::fromStdString(name) + " (" + QString::fromStdString(id) + ")";
-      userInfoDialog* dialog = new userInfoDialog(this,
+      
+      UserInfoDialog* dialog = new UserInfoDialog(this,
                                                   QString::fromStdString(registrar),
                                                   QString::fromStdString(referrer),
                                                   QString::fromStdString(lifetime_referrer),
                                                   QString::fromStdString(network_fee_percentage),
                                                   QString::fromStdString(lifetime_referrer_fee_percentage),
                                                   QString::fromStdString(referrer_rewards_percentage),
-                                                  title);
+                                                  QString::fromStdString(name),
+                                                  QString::fromStdString(id));
       dialog->setAttribute(Qt::WA_DeleteOnClose);
       dialog->open();
-      
-               
-      
-//      QZebraWidget* info_window = new QZebraWidget(QString::fromStdString(name) + " (" + QString::fromStdString(id) + ")");
-//      
-//      info_window->AddInfo(tr("Registrar"), registrar);
-//      info_window->AddInfo(tr("Referrer"), referrer);
-//      info_window->AddInfo(tr("Lifetime Referrer"), lifetime_referrer);
-//      info_window->AddInfo(tr("Network Fee"), network_fee_percentage);
-//      info_window->AddInfo(tr("Lifetime Referrer Fee"), lifetime_referrer_fee_percentage);
-//      info_window->AddInfo(tr("Referrer Rewards Percentage"), referrer_rewards_percentage);
-//
-//      info_window->setWindowTitle(QString::fromStdString(name) + " (" + QString::fromStdString(id) + ")");
-//      info_window->setFixedSize(620,420);
-//      info_window->show();
    } catch(...) {
       // Ignore for now
    }
