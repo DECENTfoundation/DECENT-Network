ed4312b14cdeb7c79cbc0f391d13fa52ea5b3a80
Hayk Poghosyan <hayq@Hayks-iMac.local>
2017-06-06 15:59:17 +0400

    FontSize

--- programs/gui_wallet/src/gui_design.cpp
+++ programs/gui_wallet/src/gui_design.cpp
@@ -22,119 +22,57 @@ QFontDatabase FontVerdanaBold()
    return resourcesFont;
 }
 
+int const FontSize()
+{
+   return
+#if defined(_WIN32)
+      8
+#elif defined (WINDOWS_HIGH_DPI)
+      8
+#elif __APPLE__
+      12
+#elif __linux__
+      10
+#else
+      12
+#endif
+   ;
+}
+
 
 QFont PaginationFont()
 {
-   return FontVerdana().font("Open Sans", "Normal",
-#if defined(_WIN32)
-        8
-#elif WINDOWS_HIGH_DPI
-        8
-#elif __APPLE__
-        12
-#elif   __linux__
-        10
-#else
-        12
-#endif
-          );
+   return FontVerdana().font("Open Sans", "Normal", FontSize());
 }
 
 QFont TableHeaderFont()
 {
-   return FontVerdanaBold().font("Open Sans", "Bold",
-
-#if defined (_WIN32)
-      8
-#elif defined(WINDOWS_HIGH_DPI)
-      8
-#elif __APPLE__
-      12
-#elif __linux__
-      10
-#else
-      14
-#endif
-      );
+   return FontVerdanaBold().font("Open Sans", "Bold", FontSize());
 }
 
 QFont AccountBalanceFont()
 {
-   return FontVerdana().font("Open Sans", "Regular",
-#if defined(_WIN32)
-      8
-#elif WINDOWS_HIGH_DPI
-      8
-#elif __APPLE__
-      12
-#elif __linux__
-      10
-#else 
-      14
-#endif
-      );
+   return FontVerdana().font("Open Sans", "Regular", FontSize());
 }
 
 QFont DescriptionDetailsFont()
 {
-   return FontVerdana().font("Open Sans", "Regular", 
-#ifdef WINDOWS_HIGH_DPI
-      8
-#else
-      14
-#endif
-      );
+   return FontVerdana().font("Open Sans", "Regular", FontSize());
 }
 
 QFont PopupButtonRegularFont()
 {
-   return FontVerdana().font("Open Sans", "Regular",
-#if defined (_WIN32)
-      8
-#elif WINDOWS_HIGH_DPI
-      8
-#elif __APPLE__
-      12
-#elif __linux__
-      10
-#else
-      14
-#endif
-      );
+   return FontVerdana().font("Open Sans", "Regular", FontSize());
 }
 
 QFont PopupButtonBigFont()
 {
-   return FontVerdana().font("Open Sans", "Regular",
-#if defined (_WIN32)
-      8
-#elif WINDOWS_HIGH_DPI
-      8
-#elif __APPLE__
-      12
-#elif __linux__
-      10
-#else
-      14
-#endif
-      );
+   return FontVerdana().font("Open Sans", "Regular", FontSize());
 }
 
 QFont TabButtonFont()
 {
-   return FontVerdana().font("Open Sans", "Regular",
-#if defined (_WIN32)
-      8
-#elif WINDOWS_HIGH_DPI
-      8
-#elif __APPLE__
-      12
-#elif __linux__
-      10
-#else
-      14
-#endif
-      );
+   return FontVerdana().font("Open Sans", "Regular", FontSize());
 }
 
 QFont MainFont()
@@ -142,7 +80,7 @@ QFont MainFont()
    return FontVerdana().font("Open Sans", "Regular",
 #if defined(_WIN32)
       14
-#elif WINDOWS_HIGH_DPI
+#elif defined (WINDOWS_HIGH_DPI)
       8
 #elif __linux__
       10
