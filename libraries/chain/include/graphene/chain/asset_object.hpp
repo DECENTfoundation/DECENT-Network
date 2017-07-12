a727fe6de4359be2d1c0d17427a5c12cc9529e6a
Josef Sevcik <josef.sevcik@gmail.com>
2017-07-12 17:27:43 +0200

    correcting in docs

--- libraries/chain/include/graphene/chain/asset_object.hpp
+++ libraries/chain/include/graphene/chain/asset_object.hpp
@@ -28,16 +28,6 @@
 #include <graphene/db/flat_index.hpp>
 #include <graphene/db/generic_index.hpp>
 
-/**
- * @defgroup prediction_market Prediction Market
- *
- * A prediction market is a specialized BitAsset such that total debt and total collateral are always equal amounts
- * (although asset IDs differ). No margin calls or force settlements may be performed on a prediction market asset. A
- * prediction market is globally settled by the issuer after the event being predicted resolves, thus a prediction
- * market must always have the @ref global_settle permission enabled. The maximum price for global settlement or short
- * sale of a prediction market asset is 1-to-1.
- */
-
 namespace graphene { namespace chain {
    class account_object;
    class database;
