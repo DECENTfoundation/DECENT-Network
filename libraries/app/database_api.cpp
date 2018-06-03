edb6537d62004462ff3e8e51b55273647b266383
James Purdy <james@eviljames.ca>
2018-06-03 17:28:01 -0600

    Add CryptoPP:: namespace declarations to fix #253

--- libraries/app/database_api.cpp
+++ libraries/app/database_api.cpp
@@ -1778,7 +1778,7 @@ namespace
       ss.calculate_secret();
       
       fc::sha256 key;
-      ss.secret.Encode((byte*)key._hash, 32);
+      ss.secret.Encode((CryptoPP::byte*)key._hash, 32);
       return key;
    }
    
@@ -1793,7 +1793,7 @@ namespace
             CryptoPP::Integer tmp(randomGenerator, 256);
             secret = tmp;
          }
-         secret.Encode((byte*)keys.key._hash, 32);
+         secret.Encode((CryptoPP::byte*)keys.key._hash, 32);
 
          uint32_t quorum = std::max((vector<account_id_type>::size_type)1, seeders.size()/3); // TODO_DECENT - quorum >= 2 see also content_submit_operation::validate
 
