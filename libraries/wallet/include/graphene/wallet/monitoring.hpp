7142dc92cbc80955f692154a6a0acd5c11ed4ba5
ghbpeter <ps@softinengines.com>
2018-11-13 15:21:36 +0100

    missing header

---
+++ libraries/wallet/include/graphene/wallet/monitoring.hpp
@@ -0,0 +1,40 @@
+/* (c) 2016, 2017 DECENT Services. For details refers to LICENSE.txt */
+/*
+ * Copyright (c) 2015 Cryptonomex, Inc., and contributors.
+ *
+ * The MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef DECENT_WALLET_MONITORING_H
+#define DECENT_WALLET_MONITORING_H
+
+/**
+ * @brief Sends an encrypted text message to one or many users.
+ * @param from account sending the message
+ * @param to account or multiple accounts receiving the message
+ * @param text the body of the message
+ * @ingroup WalletAPI_Messaging
+ */
+void reset_counters(const std::vector<std::string>& names);
+std::vector<monitoring::counter_item_cli> get_counters(const std::vector<std::string>& names);
+
+
+#endif //DECENT_WALLET_MONITORING_H
