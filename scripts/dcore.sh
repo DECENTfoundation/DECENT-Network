90a93c39060adb73ab96141d9d546da01809e29e
Richard Fric <richard.fric@decent.ch>
2019-01-15 09:11:04 +0100

    Publish container ports

--- scripts/dcore.sh
+++ scripts/dcore.sh
@@ -3,9 +3,11 @@
 [ $# -lt 2 ] && { echo "Usage: $0 image_name data_dir [genesis_json]"; exit 1; }
 
 if [ $# -lt 3 ]; then
-    docker run -d --name DCore --mount type=bind,src=$2,dst=/root/.decent/data $1
+    docker run -d --rm --name DCore -p 8090:8090 -p 40000:40000 \
+        --mount type=bind,src=$2,dst=/root/.decent/data $1
 else
-    docker run -d --name DCore --mount type=bind,src=$2,dst=/root/.decent/data \
+    docker run -d --rm --name DCore -p 8090:8090 -p 40000:40000 \
+        --mount type=bind,src=$2,dst=/root/.decent/data \
         --mount type=bind,src=$3,dst=/root/.decent/genesis.json,readonly \
         -e "DCORE_EXTRA_ARGS=--genesis-json /root/.decent/genesis.json" $1
 fi
