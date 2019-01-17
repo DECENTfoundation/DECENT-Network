5334c40b0575472a76fe8032d051ac20aa6f0f4d
David Chalupa <david.chalupa@decent.ch>
2019-01-17 13:11:18 +0100

    Using pre-built PBC packages in the Ubuntu image.

--- scripts/ubuntu/build.sh
+++ scripts/ubuntu/build.sh
@@ -37,37 +37,12 @@ if [[ $1 == "16.04" ]]; then
    cd ../..
 fi
 
-# build PBC
-wget https://crypto.stanford.edu/pbc/files/pbc-$PBC_VERSION.tar.gz
-tar xvf pbc-$PBC_VERSION.tar.gz
-cd pbc-$PBC_VERSION
-./setup
-./configure --prefix=/usr --libdir=/usr/lib/x86_64-linux-gnu
-make install
-rm -rf pbc-$PBC_VERSION pbc-$PBC_VERSION.tar.gz
+# install PBC
+wget https://github.com/DECENTfoundation/pbc/releases/download/0.5.14/libpbc_0.5.14-ubuntu18.04_amd64.deb
+wget https://github.com/DECENTfoundation/pbc/releases/download/0.5.14/libpbc-dev_0.5.14-ubuntu18.04_amd64.deb
 
-cd ..
-mkdir -p libpbc/usr/lib/x86_64-linux-gnu
-cp /usr/lib/x86_64-linux-gnu/libpbc.* libpbc/usr/lib/x86_64-linux-gnu
-
-mkdir -p libpbc/DEBIAN
-echo "Package: libpbc" > libpbc/DEBIAN/control
-echo "Version: $PBC_VERSION" >> libpbc/DEBIAN/control
-echo "Maintainer: DECENT <support@decent.ch>" >> libpbc/DEBIAN/control
-echo "Homepage: https://crypto.stanford.edu/pbc" >> libpbc/DEBIAN/control
-echo "Source: https://crypto.stanford.edu/pbc/files/pbc-$PBC_VERSION.tar.gz" >> libpbc/DEBIAN/control
-echo "Section: net" >> libpbc/DEBIAN/control
-echo "Priority: optional" >> libpbc/DEBIAN/control
-echo "Architecture: amd64" >> libpbc/DEBIAN/control
-echo "Description: Pairing-Based Crypto library." >> libpbc/DEBIAN/control
-echo " Pairing-based cryptography is a relatively young area of cryptography that" >> libpbc/DEBIAN/control
-echo " revolves around a certain function with special properties. The PBC library is" >> libpbc/DEBIAN/control
-echo " designed to be the backbone of implementations of pairing-based cryptosystems," >> libpbc/DEBIAN/control
-echo " thus speed and portability are important goals. It provides routines such as" >> libpbc/DEBIAN/control
-echo " elliptic curve generation, elliptic curve arithmetic and pairing computation." >> libpbc/DEBIAN/control
-
-dpkg-deb --build libpbc dcore-deb
-rm -rf libpbc
+dpkg -i libpbc_0.5.14-ubuntu18.04_amd64.deb
+dpkg -i libpbc-dev_0.5.14-ubuntu18.04_amd64.deb
 
 # build DCore
 git clone --single-branch --branch $GIT_REV https://github.com/DECENTfoundation/DECENT-Network.git
