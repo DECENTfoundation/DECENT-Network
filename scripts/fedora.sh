3a2d68d55d634a77466c46facac21fdaf5990e4c
David Chalupa <david.chalupa@decent.ch>
2019-01-18 14:51:30 +0100

    Added the possibility to customize the package directory.

--- scripts/fedora.sh
+++ scripts/fedora.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-[ $# -lt 2 ] && { echo "Usage: $0 image_version dcore_version [git_revision]"; exit 1; }
+[ $# -lt 2 ] && { echo "Usage: $0 image_version dcore_version [git_revision] [package_dir]"; exit 1; }
 
 if [ $# -lt 3 ]; then GIT_REV=$2; else GIT_REV=$3; fi
+
+if [ $# -lt 4 ]; then PACKAGE_DIR="$PWD/packages"; else PACKAGE_DIR=$4; fi
 
 IMAGE=`docker images -q decent/fedora/build:$1`
 if [ -z $IMAGE ]; then
@@ -13,7 +15,7 @@ else
 fi
 
 docker run -it -w /root --rm --name fedora.build.$1 \
-    --mount type=bind,src=$PWD/packages,dst=/root/rpmbuild/RPMS/x86_64 \
+    --mount type=bind,src=$PACKAGE_DIR,dst=/root/rpmbuild/RPMS/x86_64 \
     --mount type=bind,src=$PWD/fedora,dst=/root/fedora,readonly \
     decent/fedora/build:$1 fedora/build.sh $2 $GIT_REV
-docker build -t decent/fedora/dcore:$2 -f fedora/Dockerfile --build-arg DCORE_VERSION=$2 --build-arg IMAGE_VERSION=$1 packages
+docker build -t decent/fedora/dcore:$2 -f fedora/Dockerfile --build-arg DCORE_VERSION=$2 --build-arg IMAGE_VERSION=$1 $PACKAGE_DIR
--- scripts/ubuntu.sh
+++ scripts/ubuntu.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-[ $# -lt 2 ] && { echo "Usage: $0 image_version dcore_version [git_revision]"; exit 1; }
+[ $# -lt 2 ] && { echo "Usage: $0 image_version dcore_version [git_revision] [package_dir]"; exit 1; }
 
 if [ $# -lt 3 ]; then GIT_REV=$2; else GIT_REV=$3; fi
+
+if [ $# -lt 4 ]; then PACKAGE_DIR="$PWD/packages"; else PACKAGE_DIR=$4; fi
 
 IMAGE=`docker images -q decent/ubuntu/build:$1`
 if [ -z $IMAGE ]; then
@@ -13,7 +15,7 @@ else
 fi
 
 docker run -it -w /root --rm --name ubuntu.build.$1 \
-    --mount type=bind,src=$PWD/packages,dst=/root/dcore-deb \
+    --mount type=bind,src=$PACKAGE_DIR,dst=/root/dcore-deb \
     --mount type=bind,src=$PWD/ubuntu,dst=/root/ubuntu,readonly \
     decent/ubuntu/build:$1 ubuntu/build.sh $2 $GIT_REV
-docker build -t decent/ubuntu/dcore:$2 -f ubuntu/Dockerfile --build-arg DCORE_VERSION=$2 --build-arg IMAGE_VERSION=$1 packages
+docker build -t decent/ubuntu/dcore:$2 -f ubuntu/Dockerfile --build-arg DCORE_VERSION=$2 --build-arg IMAGE_VERSION=$1 $PACKAGE_DIR
