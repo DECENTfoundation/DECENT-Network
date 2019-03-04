582e05617eedae07fab5fac58b842df9ac1a0ed3
Richard Fric <richard.fric@decent.ch>
2019-03-04 08:24:43 +0100

    Use binary JSON to pack/unpack object

--- libraries/utilities/include/decent/json.hpp
+++ libraries/utilities/include/decent/json.hpp
@@ -12,14 +12,17 @@ namespace decent {
    std::string json_to_string(const json_t &doc);
    json_t string_to_json(const std::string &doc);
 
+   std::vector<uint8_t> json_to_binary(const json_t &doc);
+   json_t binary_to_json(const std::vector<uint8_t> &doc);
+
 }
 
 FC_REFLECT_TYPENAME( decent::json_t )
 
 namespace fc {
 
-   void to_variant( const decent::json_t& o, variant& v );
-   void from_variant( const variant& v, decent::json_t& o );
+   void to_variant(const decent::json_t& o, variant& v);
+   void from_variant(const variant& v, decent::json_t& o);
 
    template<> struct reflector<decent::json_t> {
       typedef decent::json_t type;
@@ -32,14 +35,14 @@ namespace fc {
 
       template<typename S, typename C>
       static void visit(const fc::raw::detail::pack_object_visitor<S, C>& v) {
-         std::function<std::string(const decent::json_t&)> f = &decent::json_to_string;
+         std::function<std::vector<uint8_t>(const decent::json_t&)> f = &decent::json_to_binary;
          v.TEMPLATE operator()("doc", f);
       }
 
       template<typename S, typename C>
       static void visit(const fc::raw::detail::unpack_object_visitor<S, C>& v) {
          try {
-            std::function<decent::json_t(const std::string&)> f = &decent::string_to_json;
+            std::function<decent::json_t(const std::vector<uint8_t>&)> f = &decent::binary_to_json;
             v.TEMPLATE operator()("doc", f);
          }
          catch(const decent::json_t::parse_error& e) {
