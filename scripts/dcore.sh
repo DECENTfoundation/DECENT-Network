eda4eb198d04af0bce200639b6477814bd2eb979
Richard Fric <richard.fric@decent.ch>
2019-04-15 14:10:52 +0200

    Run docker image as non root user

--- scripts/dcore.sh
+++ scripts/dcore.sh
@@ -3,7 +3,7 @@
 [ $# -lt 2 ] && { echo "Usage: $0 image_name data_dir [container_path]"; exit 1; }
 
 if [ $# -lt 3 ]; then
-    docker run -d --rm --name DCore p 8090:8090 -p 40000:40000 \
+    docker run -d --rm --name DCore -p 8090:8090 -p 40000:40000 \
         --mount type=bind,src=$2,dst=/root/.decent/data $1
 else
     docker run -d --rm --name DCore -p 8090:8090 -p 40000:40000 \
