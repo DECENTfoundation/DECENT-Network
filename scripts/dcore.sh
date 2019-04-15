5e6668dfa6d7b9f9ebdb105626fa71198c341416
Richard Fric <richard.fric@decent.ch>
2019-04-15 14:53:55 +0200

    Correct docker mapping of data dir for non root user

--- scripts/dcore.sh
+++ scripts/dcore.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-[ $# -lt 2 ] && { echo "Usage: $0 image_name data_dir [container_path]"; exit 1; }
+[ $# -lt 2 ] && { echo "Usage: $0 image_name data_dir [container_user [container_home]]"; exit 1; }
+
+if [ $# -lt 3 ]; then CONTAINER_USER=dcore; else CONTAINER_USER=$3; fi
+
+if [ $# -lt 4 ]; then CONTAINER_HOME=/home/$CONTAINER_USER; else CONTAINER_HOME=$4; fi
 
 if [ $# -lt 3 ]; then
     docker run -d --rm --name DCore -p 8090:8090 -p 40000:40000 \
-        --mount type=bind,src=$2,dst=/root/.decent/data $1
+        --mount type=bind,src=$2,dst=$CONTAINER_HOME/.decent/data $1
 else
     docker run -d --rm --name DCore -p 8090:8090 -p 40000:40000 \
-        --mount type=bind,src=$2,dst=$3 -e "DCORE_DATA_DIR=$3" $1
+        --mount type=bind,src=$2,dst=$CONTAINER_HOME/.decent/data \
+        -e "DCORE_USER=$CONTAINER_USER" -e "DCORE_HOME=$CONTAINER_HOME" $1
 fi
--- scripts/dcore_custom_net.sh
+++ scripts/dcore_custom_net.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-[ $# -lt 3 ] && { echo "Usage: $0 image_name data_dir genesis_json [container_path]"; exit 1; }
+[ $# -lt 3 ] && { echo "Usage: $0 image_name data_dir genesis_json [container_user [container_home]]"; exit 1; }
+
+if [ $# -lt 4 ]; then CONTAINER_USER=dcore; else CONTAINER_USER=$4; fi
+
+if [ $# -lt 5 ]; then CONTAINER_HOME=/home/$CONTAINER_USER; else CONTAINER_HOME=$5; fi
+
+GENESIS_JSON_PATH=$CONTAINER_HOME/.decent/genesis.json
 
 if [ $# -lt 4 ]; then
     docker run -d --rm --name DCore -p 8090:8090 -p 40000:40000 \
-        --mount type=bind,src=$2,dst=/root/.decent/data \
-        --mount type=bind,src=$3,dst=/root/.decent/genesis.json,readonly \
-        -e "DCORE_EXTRA_ARGS=--genesis-json /root/.decent/genesis.json" $1
-else
-    GENESIS_JSON_PATH="$4/../genesis.json"
-    docker run -d --rm --name DCore -p 8090:8090 -p 40000:40000 \
-        --mount type=bind,src=$2,dst=$4 \
+        --mount type=bind,src=$2,dst=$CONTAINER_HOME/.decent/data \
         --mount type=bind,src=$3,dst=$GENESIS_JSON_PATH,readonly \
-        -e "DCORE_EXTRA_ARGS=--genesis-json $GENESIS_JSON_PATH" -e DCORE_DATA_DIR=$4 $1
+        -e "DCORE_EXTRA_ARGS=--genesis-json $GENESIS_JSON_PATH" $1
+else
+    docker run -d --rm --name DCore -p 8090:8090 -p 40000:40000 \
+        --mount type=bind,src=$2,dst=$CONTAINER_HOME/.decent/data \
+        --mount type=bind,src=$3,dst=$GENESIS_JSON_PATH,readonly \
+        -e "DCORE_EXTRA_ARGS=--genesis-json $GENESIS_JSON_PATH" \
+        -e "DCORE_USER=$CONTAINER_USER" -e "DCORE_HOME=$CONTAINER_HOME" $1
 fi
