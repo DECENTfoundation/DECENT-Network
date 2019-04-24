98482c4379bc5128323a6b66ac06572576e38ad0
Richard Fric <richard.fric@decent.ch>
2019-04-24 09:08:40 +0200

    Move Docker scripts to DCore-Docker repository

--- scripts/cli_wallet.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-[ $# -lt 1 ] && { echo "Usage: $0 wallet_file [container_name]"; exit 1; }
-
-if [ $# -lt 2 ]; then CONTAINER_NAME=DCore; else CONTAINER_NAME=$2; fi
-
-docker cp $1 $CONTAINER_NAME:/root/.decent/wallet.json
-docker exec -it -w /root $CONTAINER_NAME cli_wallet
-docker cp $CONTAINER_NAME:/root/.decent/wallet.json $1
