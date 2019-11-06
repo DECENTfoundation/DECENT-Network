385880ee8889561f2715407dd03d8dcf6c0d87ef
Richard Fric <richard.fric@decent.ch>
2019-11-06 23:06:33 +0100

    Remove Git hash/timestamp and build date from about info

--- libraries/utilities/about.cpp
+++ libraries/utilities/about.cpp
@@ -1,19 +1,11 @@
 #include <decent/about.hpp>
+#include <graphene/utilities/git_revision.hpp>
 
 #include <boost/lexical_cast.hpp>
-#include <boost/version.hpp>
 #include <boost/algorithm/string/replace.hpp>
-#include <boost/algorithm/string.hpp>
-#include <boost/program_options/parsers.hpp>
 
 #include <openssl/opensslv.h>
 #include <cryptopp/config.h>
-
-#include <fc/git_revision.hpp>
-#include <fc/time.hpp>
-#include <fc/variant_object.hpp>
-
-#include <graphene/utilities/git_revision.hpp>
 
 #include <iostream>
 
@@ -39,23 +31,13 @@ namespace decent {
       return cryptopp_version_text;
    }
 
-   about_info get_about_daemon()
+   about_info get_about_info()
    {
-      std::string version( graphene::utilities::git_revision_description );
-      const size_t pos = version.find( '/' );
-      if( pos != std::string::npos && version.size() > pos )
-         version = version.substr( pos + 1 );
-
       return {
-         version,
-         graphene::utilities::git_revision_sha,
-         fc::get_approximate_relative_time_string( fc::time_point_sec( graphene::utilities::git_revision_unix_timestamp ) ),
-         fc::git_revision_sha,
-         fc::get_approximate_relative_time_string( fc::time_point_sec( fc::git_revision_unix_timestamp ) ),
-         "compiled on " __DATE__ " at " __TIME__,
-         decent::get_boost_version(),
+         graphene::utilities::git_version(),
+         get_boost_version(),
          OPENSSL_VERSION_TEXT,
-         decent::get_cryptopp_version(),
+         get_cryptopp_version(),
 #if defined(__APPLE__)
          "osx "
 #elif defined(__linux__)
@@ -69,47 +51,27 @@ namespace decent {
       };
    }
 
-   about_info get_about_wallet()
+   void dump_version_info()
    {
-      std::string version( graphene::utilities::git_revision_description );
-      const size_t pos = version.find( '/' );
-      if( pos != std::string::npos && version.size() > pos )
-         version = version.substr( pos + 1 );
-
-      return {
-         version,
-         graphene::utilities::git_revision_sha,
-         fc::get_approximate_relative_time_string( fc::time_point_sec( graphene::utilities::git_revision_unix_timestamp ) ),
-         fc::git_revision_sha,
-         fc::get_approximate_relative_time_string( fc::time_point_sec( fc::git_revision_unix_timestamp ) ),
-         "compiled on " __DATE__ " at " __TIME__,
-         decent::get_boost_version(),
-         OPENSSL_VERSION_TEXT,
-         decent::get_cryptopp_version(),
-#if defined(__APPLE__)
-         "osx "
-#elif defined(__linux__)
-         "linux "
-#elif defined(_MSC_VER)
-         "win32 "
-#else
-         "other "
+      std::cout << "DECENT Daemon " << graphene::utilities::git_version();
+#ifndef NDEBUG
+      std::cout << " (debug)";
 #endif
-         + boost::lexical_cast<std::string>(8 * sizeof(int*)) + "-bit"
-      };
+      std::cout << "\nBoost " << get_boost_version() << "\n" << get_openssl_version() << "\nCryptopp " << get_cryptopp_version() << std::endl;
    }
 
    bool check_unrecognized(const boost::program_options::parsed_options& optparsed)
    {
       std::vector<std::string> unrecognized_args = boost::program_options::collect_unrecognized(optparsed.options, boost::program_options::include_positional);
+      if(unrecognized_args.empty())
+         return false;
 
-      for (auto const& item : unrecognized_args)
-      {
-         std::cout << "Unrecognized argument: " << item << std::endl;
-         return true;
-      }
+      std::cerr << "Unrecognized argument(s):";
+      for(auto const& item : unrecognized_args)
+         std::cerr << ' ' << item;
 
-      return false;
+      std::cerr << std::endl;
+      return true;
    }
 
 } //namespace decent
--- libraries/utilities/include/decent/about.hpp
+++ libraries/utilities/include/decent/about.hpp
@@ -1,30 +1,20 @@
 #pragma once
-
-#include <boost/program_options.hpp>
-
 #include <fc/reflect/reflect.hpp>
+#include <boost/program_options/parsers.hpp>
 
 namespace decent {
 
-   std::string get_boost_version();
-   std::string get_openssl_version();
-   std::string get_cryptopp_version();
-
    struct about_info {
-      std::string version = "1.4.0";
-      std::string graphene_revision;
-      std::string graphene_revision_age;
-      std::string fc_revision;
-      std::string fc_revision_age;
-      std::string compile_date;
+      std::string version;
       std::string boost_version;
       std::string openssl_version;
       std::string cryptopp_version;
       std::string build;
    };
 
-   about_info get_about_daemon();
-   about_info get_about_wallet();
+   about_info get_about_info();
+
+   void dump_version_info();
 
    bool check_unrecognized(const boost::program_options::parsed_options& optparsed);
 
@@ -32,11 +22,6 @@ namespace decent {
 
 FC_REFLECT( decent::about_info,
             (version)
-            (graphene_revision)
-            (graphene_revision_age)
-            (fc_revision)
-            (fc_revision_age)
-            (compile_date)
             (boost_version)
             (openssl_version)
             (cryptopp_version)
